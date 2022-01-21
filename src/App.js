1adc82bfc51545b6595d53ef58bd7b033fea2f45
raiden <raiden@somosmakers.co>
2022-01-21 19:11:53 -0400

    Add Budgets functionalities created

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Container, Stack } from 'react-bootstrap';
 import BudgetCard from './components/BudgetCard';
+import AddBudgetModal from './components/AddBudgetModal';
+import { useState } from 'react'
+import { useBudgets } from './contexts/BudgetsContext';
 
 function App() {
-  return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const { budgets, getBudgetExpenses } = useBudgets()
 
-      <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start" }}>
-        <BudgetCard
-          name="Entertainment"
-          gray
-          amount={800}
-          max={1000}
-        ></BudgetCard>
-      </div>
-    </Container>
+  return (
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start" }}>
+          {budgets.map(budget => {
+            const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+
+            return (
+              <BudgetCard
+                key={budget.id}
+                name={budget.name}
+                amount={amount}
+                max={budget.max}
+              />
+            )
+          })}
+
+        </div>
+      </Container>
+
+      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)} />
+    </>
   );
 }
 
