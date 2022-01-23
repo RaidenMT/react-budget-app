a48a68ae179af6b3ecee62b21e9276d71f6479e0
raiden <raiden@somosmakers.co>
2022-01-22 23:30:41 -0400

    TotalBudgetCard created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddExpenseModal from './components/AddExpenseModal';
 import { useState } from 'react'
 import { useBudgets } from './contexts/BudgetsContext';
 import UncategorizedBudgetCard from './components/UncategorizedBudgetCard';
+import TotalBudgetCard from './components/TotalBudgetCard';
 
 function App() {
   const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
@@ -43,7 +44,8 @@ function App() {
             )
           })}
 
-          <UncategorizedBudgetCard />
+          <UncategorizedBudgetCard onAddExpenseClick={openAddExpenseModal} />
+          <TotalBudgetCard />
 
         </div>
       </Container>
